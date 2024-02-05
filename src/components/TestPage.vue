078738bd475e95434eb8db9a7fcb58f836d4c036
bstuddard <barrettstuddard@gmail.com>
2024-02-04 22:02:54 -0600

    Base Layout

--- src/components/TestPage.vue
+++ src/components/TestPage.vue
@@ -3,11 +3,14 @@
 
     <!--Main Content Body-->
     <div class="flex-grow">
-        <!--max-w-3xl default width and bg-white default background-->
-        <div class="mx-auto min-w-full sm:min-w-min text-standard-700 bg-standardb">
+        <div class="mx-auto min-w-full sm:min-w-min text-standard-700 bg-standardb min-h-svh">
             
-            <div class="bg-standard-500">
-                Test
+            <div class="text-lg mx-auto py-4 w-full text-center text-standard-500">
+                Examples of various vue/tailwind components
+            </div>
+
+            <div class="text-md mx-auto font-semibold py-4 w-full text-center text-standard-600">
+                Buttons
             </div>
 
         </div>
